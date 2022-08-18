b126da0e893c03d71fa8f6542d672e5e79671791
turgutersozmobitek <turgut.ersoz@mobitek.com>
2022-08-19 01:01:06 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -68,12 +68,6 @@ variable "vm_memory" {
     default     = "list"
 }
 
-variable "vdc_storage_name" {
-    type        = string
-    description = "Storage Policy:*"
-    default     = "list"
-}
-
 variable "vm_disk_size" {
     type        = string
     description = "Disk:*"
